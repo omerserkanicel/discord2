cbd3a08d3a569c24e7d97271314ea2a22fd25099
omerserkanicel <135071032+omerserkanicel@users.noreply.github.com>
2023-07-03 22:26:28 +0300

    Create discord2.py

---
+++ discord2.py
@@ -0,0 +1,57 @@
+import discord
+from discord.ext import commands
+import os
+import random
+import webbrowser as web
+import vlc
+import time
+import requests
+
+# ayricaliklar (intents) değişkeni botun ayrıcalıklarını depolayacak
+
+intents = discord.Intents.default()
+
+# Mesajları okuma ayrıcalığını etkinleştirelim
+
+intents.message_content = True
+
+# istemci (client) değişkeniyle bir bot oluşturalım ve ayrıcalıkları ona aktaralım
+
+client = discord.Client(intents=intents)
+       
+
+client = commands.Bot(intents=intents,command_prefix="!")
+
+@client.command()
+async def mem_at(ctx):
+    secilen_dosya = random.choice(os.listdir('images'))
+
+    f = open(f'images/{secilen_dosya}', "rb")
+
+    mem = discord.File(f)
+
+    await ctx.send(file=mem)
+
+@client.command()
+async def müzik_at(ctx):
+    secilen_dosya = random.choice(os.listdir('musics'))
+
+    f = open(f'musics/{secilen_dosya}', "rb")
+
+    musics = discord.File(f)
+
+    await ctx.send(file=musics)
+
+def get_it_image_url():    
+    url = 'https://random.dog/woof.json'
+    res = requests.get(url)
+    data = res.json() 
+    return data['url']
+   
+
+@client.command('it')
+async def it(ctx):
+    '''duck komutunu çağırdığımızda, program ordek_resmi_urlsi_al fonksiyonunu çağırır.'''
+    image_url = get_it_image_url()
+    await ctx.send(image_url)
+
